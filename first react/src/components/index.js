839a6f40e1cbd27adc32a5f8b7f43054ebfff77f
Mohitrawal7 <rawalmohit12@gmail.com>
2024-07-18 17:20:52 +0545

    first react

---
+++ first react/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import HeroSection from "./HeroSection";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonial,
+    HeroSection
+}
